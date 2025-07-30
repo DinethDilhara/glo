8c5db0fae7b1d8d3e43c17517bca73841a0707c1
Dineth De Alwis <dinethdilhara66@gmail.com>
2025-07-30 12:56:03 +0530

    build: initialize Go module with Cobra dependency

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 /*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
+Copyright © 2025 Dineth De Alwis <dinethdinlhara66@gmail.com>
 */
 package main
 
 import "github.com/DinethDilhara/glo/cmd"
 
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 func main() {
+	cmd.SetVersionInfo(version, commit, date)
 	cmd.Execute()
 }
